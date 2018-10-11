bd9a84ab1631303011fbd564905df68e725bb7f1
Grant Nestor <grantnestor@gmail.com>
2018-10-11 10:30:56 -0700

    Use prettier for code formatting

--- example/index.js
+++ example/index.js
@@ -1,18 +1,30 @@
-
-import immutable from 'immutable'
-import StickyInlines from '..'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import initialValue from './value'
-import { Value } from 'slate'
-import { Editor } from 'slate-react'
+import immutable from 'immutable';
+import StickyInlines from '..';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import initialValue from './value';
+import { Value } from 'slate';
+import { Editor } from 'slate-react';
 
 class Example extends React.Component {
   plugins = [
-    StickyInlines({ bannedTypes: ["banned", "no-sticky-boundary-link", "cant-be-empty-link", "doesnt-stick-on-delete-link"] }),
-    StickyInlines({ allowedTypes: ["no-sticky-boundary-link"], hasStickyBoundaries: false }),
-    StickyInlines({ allowedTypes: ["cant-be-empty-link"], canBeEmpty: false }),
-    StickyInlines({ allowedTypes: ["doesnt-stick-on-delete-link"], stickOnDelete: false })
+    StickyInlines({
+      bannedTypes: [
+        'banned',
+        'no-sticky-boundary-link',
+        'cant-be-empty-link',
+        'doesnt-stick-on-delete-link'
+      ]
+    }),
+    StickyInlines({
+      allowedTypes: ['no-sticky-boundary-link'],
+      hasStickyBoundaries: false
+    }),
+    StickyInlines({ allowedTypes: ['cant-be-empty-link'], canBeEmpty: false }),
+    StickyInlines({
+      allowedTypes: ['doesnt-stick-on-delete-link'],
+      stickOnDelete: false
+    })
   ];
 
   state = {
@@ -20,33 +32,53 @@ class Example extends React.Component {
   };
 
   onChange = ({ value }) => {
-    this.setState({ value })
-  }
+    this.setState({ value });
+  };
 
-  renderNode = (props) => {
+  renderNode = props => {
     switch (props.node.type) {
-      case "link": {
-        const className = props.isSelected ? "focused" : ""
-        return <a className={className} href="/" {...props.attributes}>{props.children}</a>
+      case 'link': {
+        const className = props.isSelected ? 'focused' : '';
+        return (
+          <a className={className} href="/" {...props.attributes}>
+            {props.children}
+          </a>
+        );
       }
-      case "no-sticky-boundary-link": {
-        const className = props.isSelected ? "focused" : ""
-        return <a className={className} href="/" {...props.attributes}>{props.children}</a>
+      case 'no-sticky-boundary-link': {
+        const className = props.isSelected ? 'focused' : '';
+        return (
+          <a className={className} href="/" {...props.attributes}>
+            {props.children}
+          </a>
+        );
       }
-      case "cant-be-empty-link": {
-        const className = props.isSelected ? "focused" : ""
-        return <a className={className} href="/" {...props.attributes}>{props.children}</a>
+      case 'cant-be-empty-link': {
+        const className = props.isSelected ? 'focused' : '';
+        return (
+          <a className={className} href="/" {...props.attributes}>
+            {props.children}
+          </a>
+        );
       }
-      case "doesnt-stick-on-delete-link": {
-        const className = props.isSelected ? "focused" : ""
-        return <a className={className} href="/" {...props.attributes}>{props.children}</a>
+      case 'doesnt-stick-on-delete-link': {
+        const className = props.isSelected ? 'focused' : '';
+        return (
+          <a className={className} href="/" {...props.attributes}>
+            {props.children}
+          </a>
+        );
       }
-      case "banned": {
-        const className = props.isSelected ? "focused banned" : "banned"
-        return <a className={className} href="/" {...props.attributes}>{props.children}</a>
+      case 'banned': {
+        const className = props.isSelected ? 'focused banned' : 'banned';
+        return (
+          <a className={className} href="/" {...props.attributes}>
+            {props.children}
+          </a>
+        );
       }
     }
-  }
+  };
 
   render = () => {
     return (
@@ -56,10 +88,10 @@ class Example extends React.Component {
         renderNode={this.renderNode}
         onChange={this.onChange}
       />
-    )
-  }
+    );
+  };
 }
 
-const example = <Example />
-const root = document.body.querySelector('main')
-ReactDOM.render(example, root)
+const example = <Example />;
+const root = document.body.querySelector('main');
+ReactDOM.render(example, root);
